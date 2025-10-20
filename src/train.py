52bfef13daafed455b107c51ee5a0406fb2a1471
Max Zhu <167700202+zhugt2019@users.noreply.github.com>
2025-10-20 14:12:50 +0200

    Reformatted train.py, again.

--- src/train.py
+++ src/train.py
@@ -5,7 +5,6 @@ import joblib
 # import pandas as pd
 from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 
 # from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression, RidgeCV
